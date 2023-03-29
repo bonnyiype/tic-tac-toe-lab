f0a4ac1b0d3abc67409c2a44deb421ebe5f741d5
bonnyiype <105000155+bonnyiype@users.noreply.github.com>
2023-03-30 00:34:25 +0100

    checkWin function added

--- script.js
+++ script.js
@@ -27,3 +27,39 @@ let state = {
     playerOColor: 'blue',
 
 }
+
+//Creating checkWin function to check the winning condition
+
+function checkWin() {
+
+    const winConditions = [
+  
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      for (let condition of winConditions){
+
+        // using destructuring assignment.
+         const [a, b, c] = condition;
+
+         if(state.board[a] && 
+            state.board[a] === state.board[b] &&
+            state.board[a] === state.board[c]){
+
+           return true;
+
+            }
+
+      }
+
+ return false;
+
+}
+
